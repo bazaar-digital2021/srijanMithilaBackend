06e638e429b26ab54de848068e013f1e774cf13a
Bazaar Digital Name <bazaar@example.com>
2025-08-14 10:44:58 +0530

    Updated razorpay.js

--- utils/razorpay.js
+++ utils/razorpay.js
@@ -1,6 +1,8 @@
 // utils/razorpay.js
 import Razorpay from "razorpay";
 import crypto from "crypto";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
